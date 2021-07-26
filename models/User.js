0575d854ce9c0d1f00edaeedef3bf3fd6ffed4cd
chasem <cpmcquown@gmail.com>
2021-07-26 16:59:36 -0700

    adds ability to hash passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,6 @@
+// bcrypt will allow us to h a user's password, making it a random string
+const bcrypt = require('bcrypt');
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -34,6 +37,18 @@ User.init(
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+  }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
